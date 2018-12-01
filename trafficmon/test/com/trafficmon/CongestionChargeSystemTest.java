62b1d1d17fdf2a72529cb7b717636e355c84ab63
ChrisDO99 <32335350+ChrisDO99@users.noreply.github.com>
2018-12-01 22:14:53 +0000

    Added Empty Tests

---
+++ trafficmon/test/com/trafficmon/CongestionChargeSystemTest.java
@@ -0,0 +1,29 @@
+package com.trafficmon;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class CongestionChargeSystemTest {
+
+    @Test
+    public void sixPoundsIfBefore2PM() {
+
+    }
+
+    @Test
+    public void fourPoundsIfAfter2PM() {
+
+    }
+
+    @Test
+    public void notChargedTwice4Hours() {
+
+    }
+
+    @Test
+    public void twelvePoundsIfLongerThan4Hours() {
+
+    }
+
+}
